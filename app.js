6a8a04c079704ac62fe22735ad3df97ff70e82b9
Awrysfab <anwarr621@gmail.com>
2021-07-01 03:43:07 +0700

    init

---
+++ app.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.APP_PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at port :${PORT}`)
+})
+
+const db = require("./app/models");
+db.sequelize.sync();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "it works!"
+  });
+});
